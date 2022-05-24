0a5c58726cbc50db27e4290f1d25f81adc1f144f
elixirevo <elixirevocation@gmail.com>
2022-05-24 23:18:46 +0900

    Updates

---
+++ precache-manifest.37fbe8c3d69f8953b62b62a4ffd7d3fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13a5d9bdec2671c7f96995ab17f04d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059e987b1fec54c4ba22",
+    "url": "/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "dfa6ab4da3d3844a7d3a",
+    "url": "/static/js/2.3e2cb5bf.chunk.js"
+  },
+  {
+    "revision": "679dc0b0298984b03ab372d214526e9a",
+    "url": "/static/js/2.3e2cb5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059e987b1fec54c4ba22",
+    "url": "/static/js/main.abff96ef.chunk.js"
+  },
+  {
+    "revision": "ae2dfafbfc2365c4ffff",
+    "url": "/static/js/runtime-main.f7c5443e.js"
+  },
+  {
+    "revision": "5abba956151d4269a2b038765b14f77b",
+    "url": "/static/media/ely-logo.5abba956.jpg"
+  }
+]);
